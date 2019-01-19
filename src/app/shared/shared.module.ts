1962e7f587fbe3c543e175f98c2da684ad1dd94b
David Houseknecht <djhouseknecht@liberty.edu>
2019-01-19 15:41:04 -0500

    Made some small changes. Added ng-bootstrap dependency

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,13 @@ import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
 import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
 	imports: [
 		RouterModule,
-		CommonModule
+		CommonModule,
+		NgbModule
 	],
 	declarations: [
 		NavBarComponent,
