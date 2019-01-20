e838e118c7ec4f2f807de97fe4f425207fbf9a45
David Houseknecht <djhouseknecht@liberty.edu>
2019-01-19 21:35:14 -0500

    Re-wrote navbar and collapsible menu

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+	public collapsed: boolean = true;
+
+	/* handles to hamburger and dropdown-menu options */
+	@ViewChild('dropdownOptions') dropdownOptions: ElementRef;
+	@ViewChild('dropdownHamburger') dropdownHamburger: ElementRef;
+
+	/* event listener to close the dropdown-menu if clicked out of it */
+  @HostListener('document:click', ['$event'])
+  private clickout(event) {
+		if (this.dropdownHamburger.nativeElement.contains(event.target)) {
+			return;
+		}
+    if (!this.dropdownOptions.nativeElement.contains(event.target)) {
+      this.collapsed = true;
+    }
+  }
+
   constructor() { }
 
   ngOnInit() {
-  }
+	}
 
 }
