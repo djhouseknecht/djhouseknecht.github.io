088d93fd8517721a0d6d22c64f5e4b56b84fd320
David Houseknecht <djhouseknecht@liberty.edu>
2019-01-20 00:02:18 -0500

    Changed the spacer

---
+++ src/app/shared/components/spacer/spacer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spacer',
+	template: ``,
+	styleUrls: ['./spacer.component.scss']
+})
+export class SpacerComponent { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { SpacerComponent } from './components/spacer/spacer.component';
 
 @NgModule({
 	imports: [
@@ -14,10 +15,12 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 	declarations: [
 		NavBarComponent,
 		FooterComponent,
+		SpacerComponent,
 	],
 	exports: [
 		NavBarComponent,
 		FooterComponent,
+		SpacerComponent,
 	]
 })
 export class SharedModule { }
